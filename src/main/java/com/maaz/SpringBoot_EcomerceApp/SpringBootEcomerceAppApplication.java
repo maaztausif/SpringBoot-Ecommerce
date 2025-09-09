942e3af16f5fb8478d432942afaac77e1885ef18
maaztausif <maaztausif48@gmail.com>
2025-09-09 21:19:35 +0200

    Project Created

---
+++ src/main/java/com/maaz/SpringBoot_EcomerceApp/SpringBootEcomerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.maaz.SpringBoot_EcomerceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcomerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcomerceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaz/SpringBoot_EcomerceApp/SpringBootEcomerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaz.SpringBoot_EcomerceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcomerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
